3c589e1b70635e82df87167d9c418fdc8cdb5834
LFzinn <luizferreiracod3@gmail.com>
2024-04-17 16:55:01 -0300

    :fix: adição do angular material para controle dos breakpoints para corrigir inconsistência ao aumentar tamanho da tela manualmente

--- src/app/shared/components-shared/header/header.component.ts
+++ src/app/shared/components-shared/header/header.component.ts
@@ -1,5 +1,8 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable, map } from 'rxjs';
+
 
 @Component({
   selector: 'app-header',
@@ -9,30 +12,43 @@ import { Router } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent  {
-  showSideBar: boolean = false
-
+  showSideBar: boolean = false;
+  isMobile: boolean = false;
   @ViewChild('header') header!:ElementRef;
   @ViewChild('navigationHeader') navigationHeader!:ElementRef;
   @ViewChild('bg') bg!:ElementRef;
   @ViewChild('arrow') arrow!:ElementRef;
 
-  constructor(private router: Router) {}
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private router: Router, private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe(Breakpoints.Small)
+    .subscribe(result => {
+      this.isMobile = !result.matches;
+      if(this.isMobile == false) {
+        this.bg.nativeElement.style.display = 'none';
+        this.arrow.nativeElement.style.display = 'none';
+      }
+    });
+  }
 
   toogleSideBar() {
-    this.showSideBar = !this.showSideBar;
-    console.log(this.showSideBar);
-    if(this.showSideBar) {
+      this.showSideBar = true;
       this.navigationHeader.nativeElement.style.marginLeft = '-10vw';
       this.navigationHeader.nativeElement.classList.add('animate-sidebar');
       this.bg.nativeElement.style.display = 'block';
       this.arrow.nativeElement.style.display = 'none';
+  }
 
-    } else {
+  closeSideBar() {
+      this.showSideBar = false;
       this.navigationHeader.nativeElement.style.marginLeft = '-100vw';
       this.navigationHeader.nativeElement.classList.remove('animate-sidebar');
       this.bg.nativeElement.style.display = 'none';
       this.arrow.nativeElement.style.display = 'block';
-    }
   }
 
   goToHome():void {

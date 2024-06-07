4145675426ad49b08372a90b307c0ddb4b48edc2
LFzinn <luizferreiracod3@gmail.com>
2024-06-07 10:45:41 -0300

    fix: adjuste lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,11 @@
 import { Routes } from '@angular/router';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { HouseDetailsPageComponent } from './pages/house-details-page/house-details-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadComponent:() => import('./pages/home-page/home-page.component').then(c => HomePageComponent)},
-  { path: 'properties', loadComponent:() => import('./pages/properties-page/properties-page.component').then(c => PropertiesPageComponent)},
-  { path: 'properties/:id', loadComponent:() => import('./pages/house-details-page/house-details-page.component').then(c => HouseDetailsPageComponent)},
-  { path: 'about', loadComponent:()=> import('./pages/about-page/about-page.component').then(c => AboutPageComponent)},
-  { path: 'contact', loadComponent:()=> import('./pages/contact/contact.component').then(c => ContactComponent)},
+  { path: 'home', loadComponent:() => import('./pages/home-page/home-page.component').then(c => c.HomePageComponent)},
+  { path: 'properties', loadComponent:() => import('./pages/properties-page/properties-page.component').then(c => c.PropertiesPageComponent)},
+  { path: 'properties/:id', loadComponent:() => import('./pages/house-details-page/house-details-page.component').then(c => c.HouseDetailsPageComponent)},
+  { path: 'about', loadComponent:()=> import('./pages/about-page/about-page.component').then(c => c.AboutPageComponent)},
+  { path: 'contact', loadComponent:()=> import('./pages/contact/contact.component').then(c => c.ContactComponent)},
   { path: '**', redirectTo: 'home' },
 ];
